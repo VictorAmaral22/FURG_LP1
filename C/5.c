9dcf6b64248805c5a89913af8afc8ae8c4c5b89d
victoramaral22 <victoramaral@gmail.com>
2024-04-03 14:41:51 -0300

    ai ai

--- C/5.c
+++ C/5.c
@@ -1,14 +1,34 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+int contarOcorrencias(char *frase, char *palavra) {
+    int ocorrencias = 0;
+    int tamanhoFrase = strlen(frase);
+    int tamanhoPalavra = strlen(palavra);
+
+    for (int i = 0; i <= tamanhoFrase - tamanhoPalavra; i++) {
+        if (strncmp(&frase[i], palavra, tamanhoPalavra) == 0) {
+            ocorrencias++;
+        }
+    }
+
+    return ocorrencias;
+}
 
 int main() {
-    int n, i, nfat;
-    printf("\nCalculo do fatorial de um numero\n");
-    printf("\nDigite um inteiro nao-negativo: ");
-    scanf("%d", &n); /* inicializacoes */
-    nfat = 1;
-    for (i = 2; i <= n; i++) {
-        nfat = nfat * i;
-    }
-    printf("O valor de %d!= %d\n", n, nfat);
+    char frase[100];
+    char palavra[20];
+
+    printf("Digite a frase: ");
+    fgets(frase, 100, stdin);
+    frase[strcspn(frase, "\n")] = '\0';
+
+    printf("Digite a palavra: ");
+    scanf("%s", palavra);
+
+    int ocorrencias = contarOcorrencias(frase, palavra);
+
+    printf("Temos que a palavra %s ocorre %d vezes na frase.\n", palavra, ocorrencias);
+
     return 0;
 }
