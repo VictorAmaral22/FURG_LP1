321ae0b54e664d460934a5e68a1d093cf198cb97
victoramaral22 <victoramaral@gmail.com>
2024-07-17 19:53:27 -0300

    uai so

---
+++ hailstone.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func hailstone(n int) int {
+	if n != 1 {
+		if n % 2 == 0 {
+			fmt.Println(n / 2)
+			return hailstone(n / 2)
+		} else {
+			fmt.Println((n * 3) + 1)
+			return hailstone((n * 3) + 1)
+		}
+	}
+	return 1
+}
+
+func main() {
+	hailstone(12)
+}
