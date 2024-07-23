65ff5216e2ba7c8612b4032e8ec6d026804e559b
victoramaral22 <victortavamaral@gmail.com>
2024-07-23 20:30:24 -0300

    ai ai

--- C/dinamic_allocation/exerc.c
+++ C/dinamic_allocation/exerc.c
@@ -2,97 +2,117 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Produto {
+struct Produto
+{
     char nome[30];
     int codigo;
     double preco;
 };
 
-typedef struct node {
+typedef struct node
+{
     struct Produto produto;
-    struct node* next;
+    struct node *next;
 } Node;
 
-Node* criarNo(struct Produto produto) {
-    Node* novoNo = (Node*)malloc(sizeof(Node));
+Node *criarNo(struct Produto produto)
+{
+    Node *novoNo = (Node *)malloc(sizeof(Node));
     novoNo->produto = produto;
     novoNo->next = NULL;
     return novoNo;
 }
 
-void adicionarProduto(Node** head) {
+void add(Node **list)
+{
     struct Produto novoProduto;
-    printf("Digite o nome do produto: ");
+    printf("\nDigite o nome do produto (sem espaços): ");
     scanf("%s", novoProduto.nome);
     printf("Digite o codigo do produto: ");
     scanf("%d", &novoProduto.codigo);
     printf("Digite o preço do produto: ");
     scanf("%lf", &novoProduto.preco);
 
-    Node* novoNo = criarNo(novoProduto);
+    Node *novoNo = criarNo(novoProduto);
 
-    if (*head == NULL) {
-        *head = novoNo;
-    } else {
-        Node* temp = *head;
-        while (temp->next != NULL) {
+    if (*list == NULL)
+    {
+        *list = novoNo;
+    }
+    else
+    {
+        Node *temp = *list;
+        while (temp->next != NULL)
+        {
             temp = temp->next;
         }
         temp->next = novoNo;
     }
 
-    printf("Produto adicionado com sucesso!\n");
+    printf("\nProduto adicionado com sucesso!\n");
 }
 
-void exibirProdutos(Node* head) {
-    if (head == NULL) {
-        printf("Nenhum produto na lista.\n");
+void show(Node *list)
+{
+    if (list == NULL)
+    {
+        printf("\nNenhum produto na lista.\n");
         return;
     }
 
-    Node* temp = head;
-    while (temp != NULL) {
+    Node *temp = list;
+    printf("\n");
+    while (temp != NULL)
+    {
         printf("Nome: %s, Codigo: %d, Preco: %.2f\n", temp->produto.nome, temp->produto.codigo, temp->produto.preco);
         temp = temp->next;
     }
 }
 
-void buscarProdutoPorNome(Node* head) {
-    if (head == NULL) {
-        printf("Nenhum produto na lista.\n");
+void search(Node *list)
+{
+    if (list == NULL)
+    {
+        printf("\nNenhum produto na lista.\n");
         return;
     }
 
     char nome[30];
-    printf("Digite o nome do produto a ser buscado: ");
+    printf("\nDigite o nome do produto a ser buscado: ");
     scanf("%s", nome);
 
-    Node* temp = head;
-    while (temp != NULL) {
-        if (strcmp(temp->produto.nome, nome) == 0) {
+    Node *temp = list;
+    while (temp != NULL)
+    {
+        if (strcmp(temp->produto.nome, nome) == 0)
+        {
             printf("Produto encontrado: Nome: %s, Código: %d, Preco: %.2f\n", temp->produto.nome, temp->produto.codigo, temp->produto.preco);
             return;
         }
         temp = temp->next;
     }
 
-    printf("Produto nao encontrado.\n");
+    printf("\nProduto nao encontrado.\n");
 }
 
-void liberarLista(Node* head) {
-    Node* temp;
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
+void freeList(Node *list)
+{
+    Node *temp;
+    while (list != NULL)
+    {
+        temp = list;
+        list = list->next;
         free(temp);
     }
 }
 
-int main() {
-    Node* head = NULL;
+int main()
+{
+    Node *list = NULL;
     int opcao;
 
-    do {
+    do
+    {
         printf("\nMenu:\n");
         printf("1. Adicionar produto\n");
         printf("2. Exibir todos os produtos\n");
@@ -101,22 +121,23 @@ int main() {
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
 
-        switch (opcao) {
-            case 1:
-                adicionarProduto(&head);
-                break;
-            case 2:
-                exibirProdutos(head);
-                break;
-            case 3:
-                buscarProdutoPorNome(head);
-                break;
-            case 4:
-                liberarLista(head);
-                printf("Saindo...\n");
-                break;
-            default:
-                printf("Opção inválida. Tente novamente.\n");
+        switch (opcao)
+        {
+        case 1:
+            add(&list);
+            break;
+        case 2:
+            show(list);
+            break;
+        case 3:
+            search(list);
+            break;
+        case 4:
+            freeList(list);
+            printf("Saindo...\n");
+            break;
+        default:
+            printf("Opção inválida. Tente novamente.\n");
         }
     } while (opcao != 4);
 
